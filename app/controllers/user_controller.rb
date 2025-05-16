bdb98cd8927b5e2024244ec10735acba1855fcd3
Bartosz Jędrzejewski <Bartosz666hub+github@gmail.com>
2025-05-16 15:32:19 +0200

    dodanie

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,6 @@
+class UserController < ApplicationController
+  def index
+    #@post = Post.all.find_by(id: current_user.id)
+    #@comment = Comment.all.find_by(id: current_user.id)
+  end
+end
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        './app/views/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/components/**/*.rb',
+        './app/assets/stylesheets/**/*.css'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
